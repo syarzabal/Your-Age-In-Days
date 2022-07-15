efcc3515c7a7361ded1ef846e690adeb603bacb2
syarzabal <syarzabal@gmail.com>
2022-07-15 17:11:37 +0200

    Fixed algorithm to make it work properly

--- src/Date.java
+++ src/Date.java
@@ -13,25 +13,15 @@ public class Date {
         return day;
     }
 
-    public void setDay(int day) {
-        this.day = day;
-    }
-
     public int getMonth() {
         return month;
-    }
-
-    public void setMonth(int month) {
-        this.month = month;
     }
 
     public int getYear() {
         return year;
     }
 
-    public void setYear(int year) {
-        this.year = year;
-    }
+
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,11 @@ public class Main {
         Date currentDate = new Date(15, 7, 2022);
         Date birthDate = new Date(25, 4, 2002);
 
-        System.out.println("You have lived "+daysLived(currentDate, birthDate)+" days.");
+        int daysLived = daysLived(currentDate, birthDate);
+        System.out.println("You have lived "+daysLived+" days.");
+        System.out.println("That's the equivalent of "+(double) daysLived/365+" years.");
     }
+
 
     private static boolean isLeapYear(int year) {
         return year % 4 == 0;
@@ -15,30 +18,36 @@ public class Main {
 
     private static int daysLived(Date currentDate, Date birthDate){
         int daysLived = 0;
+        int[] regularYearMonthDays = new int[] {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int[] leapYearMonthDays = new int[] {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-        //Adding the days in each year
-        for(int auxYear = birthDate.getYear(); auxYear < currentDate.getYear() ; auxYear++){
-            if(isLeapYear(auxYear))
+        int[] birthYearMonthDays = isLeapYear(birthDate.getYear())? leapYearMonthDays : regularYearMonthDays;
+        int[] currentYearMonthDays = isLeapYear(currentDate.getYear())? leapYearMonthDays : regularYearMonthDays;
+
+        //Calculating days lived until end of birth year
+        daysLived += birthYearMonthDays[birthDate.getMonth()] - birthDate.getDay();
+        for(int month = birthDate.getMonth() + 1 ;  month <= 12 ; month++){
+            daysLived += birthYearMonthDays[month];
+        }
+
+        //Calculating days lived in the years in between birth and current year
+        for(int year = birthDate.getYear() + 1 ; year < currentDate.getYear() ; year++){
+            if(isLeapYear(year))
                 daysLived += 366;
             else
                 daysLived += 365;
         }
 
-        int[] currentYearMonthDays;
-        if(isLeapYear(currentDate.getYear()))
-            currentYearMonthDays = new int[] {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        else
-            currentYearMonthDays = new int[] {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-        int auxMonth;
-        for(auxMonth = 1 ; auxMonth < currentDate.getMonth() ; auxMonth++){
-            daysLived += currentYearMonthDays[auxMonth];
+        //Calculating days lived in the current year
+        for(int month = 1 ; month < currentDate.getMonth() ; month++){
+            daysLived += currentYearMonthDays[month];
         }
-
         daysLived += currentDate.getDay();
 
         return daysLived;
     }
+
+
 
 }
 
